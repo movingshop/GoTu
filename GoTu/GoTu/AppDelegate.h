811fc8aa35dc25b28fa6b3a3f66dbfc95dbd2487
movignshop <vince.movingshop.org>
2013-11-26 14:29:42 +0800

    01

---
+++ GoTu/GoTu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoTu
+//
+//  Created by xuwenjuan on 13-11-26.
+//  Copyright (c) 2013年 vince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoTu/GoTuTests/GoTuTests.m
@@ -0,0 +1,34 @@
+//
+//  GoTuTests.m
+//  GoTuTests
+//
+//  Created by xuwenjuan on 13-11-26.
+//  Copyright (c) 2013年 vince. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoTuTests : XCTestCase
+
+@end
+
+@implementation GoTuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
